5a718c30e1cb19dbb8773badd67553a6c7f99900
J Burns <j.burns2372@gmail.com>
2026-02-25 11:27:49 +0000

    the web preview is not loading

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,26 +1,25 @@
 { pkgs, ... }: {
-  # See https://developers.google.com/idx/guides/customize-idx-env
-  # for more details on customizing your environment.
   channel = "stable-23.11";
+  
   packages = [
     pkgs.nodejs_20
   ];
-  previews = {
-    enable = true;
-    previews = {
-      web = {
-        command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
-        manager = "web";
+  
+  idx = {
+    workspace = {
+      onCreate = {
+        npm-install = "npm install";
       };
     };
-  };
-  workspace = {
-    onCreate = [
-      "npm install"
-    ];
-    onStart = [
-      # The following command will be run in the background when your workspace starts
-      # "npm run genkit:watch &"
-    ];
+    
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
   };
 }
