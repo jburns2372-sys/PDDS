47bca137677e0fb4c7be9567aa7b7ea8a96efa1e
J Burns <j.burns2372@gmail.com>
2026-02-25 11:51:28 +0000

    npm run dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  experimental: {
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+        ],
+    },
+    experimental: {
+        allowedDevOrigins: [
+            "https://6000-firebase-studio-1771944132599.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev"
+        ],
+    },
 };
 
 export default nextConfig;
