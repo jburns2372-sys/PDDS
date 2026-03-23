2de277aed63522c352f69a4616e5aa730e1e4e2a
jburns2372-sys <j.burns2372@gmail.com>
2026-03-23 19:03:24 +0800

    Wiping all files from GitHub to start fresh

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function RootPage() {
-  // This page just redirects to the home page.
-  // The auth guard in the AppShell will handle redirecting to /login if needed.
-  redirect('/home');
-}
